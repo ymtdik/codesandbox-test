90bb321979ef7699a30614c9fab03bba160e1f9e
ymtdik <45428846+ymtdik@users.noreply.github.com>
2020-11-24 21:49:19 +0900

    論理演算子

--- src/index.js
+++ src/index.js
@@ -169,12 +169,52 @@ const nameArr = ["田中", "山田", "じゃけぇ"];
 // });
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "じゃけぇ") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
 
-console.log(newNameArr);
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時　：　条件がfalseの時
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum)
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています！" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 60));
+
+/**
+ * 論理演算子の本当の意味を知ろう
+ */
+const flag1 = true;
+const flag2 = false;
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+if (flag1 && flag2) {
+  console.log("1も2もtrueになります");
+}
+
+// || は左側がfalseなら右側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す
+const num2 = 100;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
